258ac7c7c71342da0d0926bb6a1c0f04ac5e06e9
sedx876 <swatkins876@gmail.com>
2020-08-14 08:41:26 -0400

    styles/mediaQueries.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
+//import InfiniteScroll from 'react-infinite-scroll-component';
+//import Nav from 'components/Nav';
+//import List from 'components/List';
+//import Grid from 'components/Grid';
+//import Loader from 'components/Loader';
+//import { layouts, themes } from 'store/app/utils';
+import { colorsDark } from 'styles/palette';
 
-function App() {
+import { Wrapper, Title } from './styles';
+
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      hAcker News Clone
-    </div>
+    <ThemeProvider theme={colorsDark}>
+      <div>
+        <Wrapper>
+          <Title>Hacker News Reader</Title>
+          {/* <List /> */}
+        </Wrapper>
+      </div>
+    </ThemeProvider>
   );
+ }
 }
 
 export default App;
