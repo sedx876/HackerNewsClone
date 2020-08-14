16ef9a15e883d6bdeeda8824434a1a7bae64ff77
sedx876 <swatkins876@gmail.com>
2020-08-14 10:02:46 -0400

    List/index.js

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ListItem from 'components/ListItem';
+
 import { ListWrapper } from './styles';
 
-const List = () => (
-  <ListWrapper>
-    <ListItem />
-  </ListWrapper>
-);
+class List extends Component {
+  static propTypes = {
+    stories: PropTypes.array.isRequired,
+  };
+
+  render() {
+    const { stories } = this.props;
+    return (
+      <ListWrapper>
+        {stories.map(story => (
+          <ListItem key={story.id} {...story} />
+        ))}
+      </ListWrapper>
+    );
+  }
+}
 
 export default List;
